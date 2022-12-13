0327bdf8b775b06f31bba99ef5d276d5b2b00820
banksinn <sintanawongsupakdee@gmail.com>
2022-12-13 19:01:29 +0700

    change next.config.js

--- next.config.js
+++ next.config.js
@@ -4,8 +4,9 @@ module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       env: {
-        MONGODB_URI: process.env.NEXT_PUBLIC_MONGODB_URI,
-        MONGODB_DB: process.env.NEXT_PUBLIC_MONGODB_DB,
+        MONGODB_URI:
+          "mongodb+srv://banksinn:b910524721@cluster0.wv1rjgl.mongodb.net/?retryWrites=true&w=majority",
+        MONGODB_DB: "my-site",
       },
     }
   }
