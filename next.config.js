5bad3a0ebb65e8ee7142496fa123cbc757b2f7a9
banksinn <sintanawongsupakdee@gmail.com>
2022-12-13 18:55:22 +0700

    change next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants")
 
-module.exports = nextConfig
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        MONGODB_URI: process.env.NEXT_PUBLIC_MONGODB_URI,
+        MONGODB_DB: process.env.NEXT_PUBLIC_MONGODB_DB,
+      },
+    }
+  }
+  return {
+    env: {
+      MONGODB_URI: process.env.NEXT_PUBLIC_MONGODB_URI,
+      MONGODB_DB: process.env.NEXT_PUBLIC_MONGODB_DB,
+    },
+  }
+}
