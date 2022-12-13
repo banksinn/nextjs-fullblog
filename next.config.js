527ac9a378749b21df612aaf90217d142031c3d3
banksinn <sintanawongsupakdee@gmail.com>
2022-12-13 20:11:24 +0700

    change next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_MONGODB_URI: process.env.NEXT_PUBLIC_MONGODB_URI,
+    NEXT_PUBLIC_MONGODB_DB: process.env.NEXT_PUBLIC_MONGODB_DB,
+  },
 }
 
 module.exports = nextConfig
