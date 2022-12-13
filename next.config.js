5c902b69f3d5dfb900c2a1634cfaa7b0a68aee77
banksinn <sintanawongsupakdee@gmail.com>
2022-12-13 19:20:17 +0700

    change next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants")
-
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      env: {
-        MONGODB_URI:
-          "mongodb+srv://banksinn:b910524721@cluster0.wv1rjgl.mongodb.net/?retryWrites=true&w=majority",
-        MONGODB_DB: "my-site",
-      },
-    }
-  }
-  return {
-    env: {
-      MONGODB_URI: process.env.NEXT_PUBLIC_MONGODB_URI,
-      MONGODB_DB: process.env.NEXT_PUBLIC_MONGODB_DB,
-    },
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 }
+
+module.exports = nextConfig
